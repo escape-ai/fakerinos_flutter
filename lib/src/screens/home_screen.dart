7043aefab36beea437dabacfefec92c725162ee5
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-02-24 09:54:57 +0800

    Carousel in home screen

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -23,7 +23,17 @@ class HomeStateScreen extends State<HomeScreen> {
         appBar: AppBar(
           title: Text("Home Page")
         ),
-      body: Text("Welcome"),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        itemBuilder: (BuildContext context, int index) {
+          if(index % 2 == 0) {
+            return _buildCarousel(context, index ~/ 2);
+          }
+          else {
+            return Divider();
+          }
+        },
+      ),
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
@@ -31,7 +41,7 @@ class HomeStateScreen extends State<HomeScreen> {
               padding:EdgeInsets.zero,
               child: Text('Fakerinos'),
               decoration: BoxDecoration(
-                color: Colors.blue,
+                
               )
             ),
             ListTile(
@@ -65,3 +75,35 @@ class HomeStateScreen extends State<HomeScreen> {
       )
     );
 } } 
+
+Widget _buildCarousel(BuildContext context, int carouselIndex) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text('Carousel $carouselIndex'),
+        SizedBox(
+          // you may want to use an aspect ratio here for tablet support
+          height: 200.0,
+          child: PageView.builder(
+            // store this controller in a State to save the carousel scroll position
+            controller: PageController(viewportFraction: 0.8),
+            itemBuilder: (BuildContext context, int itemIndex) {
+              return _buildCarouselItem(context, carouselIndex, itemIndex);
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildCarouselItem(BuildContext context, int carouselIndex, int itemIndex) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 4.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blueGrey,
+          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+        ),
+      ),
+    );
+  }
