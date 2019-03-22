e4b6057d440ec34523e6fabc4a71cdcd12db9cd4
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-03-22 10:46:38 +0800

    counter logic + counter test

---
+++ lib/src/logic/counter.dart
@@ -0,0 +1,22 @@
+// class Counter {
+
+//   int value; 
+//   int gameDuration; 
+
+//   Counter(int initialValue, int gameDuration){
+//     this.value = initialValue; 
+//     this.gameDuration = gameDuration;
+
+//     assert((gameDuration >= 30), 
+//       "Minimum initial game duration time is 30 seconds");
+//     }
+  
+//   void increment() {
+//     if 
+//   }
+
+
+//   void increment() => value++;
+
+//   void decrement() => value--;
+// }
---
+++ test/counter_test.dart
@@ -0,0 +1,13 @@
+// import '../lib/src/logic/counter.dart';
+// // import 'package:/counter.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('Counter value should be incremented', () {
+//     final counter = Counter();
+
+//     counter.increment();
+
+//     expect(counter.value, 1);
+//   });
+// }
