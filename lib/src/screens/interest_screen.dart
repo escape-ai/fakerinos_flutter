10a907aade07f540d5524cfb0938335d804d9d67
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-04-11 02:02:21 +0800

    interest screen can now do get request. Interesting fact - HTTPHeaders is provided by the dart.io library

--- lib/src/screens/interest_screen.dart
+++ lib/src/screens/interest_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart'; 
+import 'dart:convert';
 import '../screens/home_screen.dart';
 import "../Session.dart";
+import 'package:http/http.dart'; 
+import 'partials/InterestCards.dart';
+import 'dart:io';
+
 
 class InterestScreen extends StatefulWidget {
 
@@ -16,15 +21,49 @@ class InterestStateScreen extends State<InterestScreen> {
 
   final List<String> _suggestions = <String>[]; 
   final Set<String> _saved = new Set<String>(); 
+  InterestCards interestCardsData; 
+
+  @override
+  void initState(){
+    super.initState();
+    print("Interest Screen Initializing");
+    _fetchData(); 
+  }
+
+  _fetchData() async {
+      print("Fetching data"); 
+      
+    final response = await get("https://fakerinos.herokuapp.com/api/articles/tag",
+    headers: {HttpHeaders.authorizationHeader: "Token 3ade3638c37c5370ab3c0679a7a8107eee133ed7"}); 
+    print(response.body); 
+    if (response.statusCode == 200) {
+      var decodedJson = new List();
+      
+      decodedJson = jsonDecode(response.body); 
+      // print(decodedJson);
+      interestCardsData = InterestCards.fromJson(decodedJson);
+
+      print("Interest Cards data output");
+      print(interestCardsData.toJson()); 
+
+      setState(() {
+      });
+    } else {
+      // print(response.statusCode);
+      throw Exception('Failed to load cards');
+    }
+  }
 
   @override
   Widget build(context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Getting Started")
+          title: Text("Tell us what interest you!")
         ),
-        body: buildGrid(),
+        body: interestCardsData == null ? 
+        CircularProgressIndicator() :
+        buildGrid(),
         floatingActionButton: FloatingActionButton.extended(
           elevation: 2.0,
           onPressed: (){
@@ -77,21 +116,8 @@ GestureDetector makeGridCell(String name, IconData icon){
       childAspectRatio: 1.0,
       mainAxisSpacing: 1.0,
       crossAxisSpacing: 1.0,
-      children: <Widget>[
-        makeGridCell("Adventures", Icons.terrain),
-        makeGridCell("Local", Icons.local_activity),
-        makeGridCell("Social", Icons.chat_bubble),
-        makeGridCell("Sports", Icons.explore),
-        makeGridCell("Adventures", Icons.terrain),
-        makeGridCell("Local", Icons.local_activity),
-        makeGridCell("Social", Icons.chat_bubble),
-        makeGridCell("Sports", Icons.explore),
-        makeGridCell("Adventures", Icons.terrain),
-        makeGridCell("Local", Icons.local_activity),
-        makeGridCell("Social", Icons.chat_bubble),
-        makeGridCell("Sports", Icons.explore),
-        
-      ]
+      children: interestCardsData.interestCards.map(
+        (card) => makeGridCell(card.name, Icons.trending_up)).toList()
     );
 
 } }
